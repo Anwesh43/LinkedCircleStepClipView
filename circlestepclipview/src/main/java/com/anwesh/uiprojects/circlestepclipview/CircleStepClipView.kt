aa84bf97f24b62770f995c520cbe2bd320bef64d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-22 09:30:26 +0530

    adding libraries and defining nodes

--- circlestepclipview/src/main/java/com/anwesh/uiprojects/circlestepclipview/CircleStepClipView.kt
+++ circlestepclipview/src/main/java/com/anwesh/uiprojects/circlestepclipview/CircleStepClipView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.circlestepclipview
 /**
  * Created by anweshmishra on 22/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
